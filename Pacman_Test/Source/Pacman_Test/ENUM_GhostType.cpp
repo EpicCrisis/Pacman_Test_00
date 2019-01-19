8315a1204aed7f6282572d79fb44bf4f0a08a96e
Terrence Chan <roxes2112@yahoo.com>
2019-01-19 17:39:49 +0800

    *Remove level cast shadows. *Add built data.

---
+++ Pacman_Test/Source/Pacman_Test/ENUM_GhostType.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ENUM_GhostType.h"
+
+ENUM_GhostType::ENUM_GhostType()
+{
+}
+
+ENUM_GhostType::~ENUM_GhostType()
+{
+}
---
+++ Pacman_Test/Source/Pacman_Test/Struct_GhostAnimations.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Struct_GhostAnimations.h"
+
+Struct_GhostAnimations::Struct_GhostAnimations()
+{
+}
+
+Struct_GhostAnimations::~Struct_GhostAnimations()
+{
+}
